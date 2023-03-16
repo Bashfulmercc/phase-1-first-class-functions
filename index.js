f3baa08eee580d99c07bb86d6e6742ac19ec3924
Mason <swartmason@gmail.com>
2023-03-15 23:52:18 -0400

    lab completed

---
+++ index.js
@@ -0,0 +1,15 @@
+function receivesAFunction(spy){
+    return spy();
+}
+
+function returnsANamedFunction(){
+    return function name(){
+
+    }
+}
+
+function returnsAnAnonymousFunction(){
+    return function(){
+
+    }
+}
